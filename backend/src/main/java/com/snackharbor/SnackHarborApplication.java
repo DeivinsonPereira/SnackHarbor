0745e218b420bdb8e2e2959b7699d1f6c12974cc
Deivinson Pereira <lordpoxtaa@gmail.com>
2023-10-31 14:33:22 -0300

    Project started

---
+++ backend/src/main/java/com/snackharbor/SnackHarborApplication.java
@@ -0,0 +1,13 @@
+package com.snackharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackHarborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackHarborApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/snackharbor/SnackHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snackharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackHarborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
